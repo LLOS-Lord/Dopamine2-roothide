a11915a74f314174fc1832a7a4c2154c4a6145b5
LLOS Lord <chungphan1607@gmail.com>
2026-08-14 17:38:49 +0000

    Fit supported iOS range in home header

--- Application/Dopamine/UI/DOHeaderView.m
+++ Application/Dopamine/UI/DOHeaderView.m
@@ -48,6 +48,10 @@
         [subtitles enumerateObjectsUsingBlock:^(NSAttributedString *formatedText, NSUInteger idx, BOOL *stop) {
             UILabel *label = [[UILabel alloc] init];
             label.attributedText = formatedText;
+            label.numberOfLines = 0;
+            label.lineBreakMode = NSLineBreakByWordWrapping;
+            label.adjustsFontSizeToFitWidth = YES;
+            label.minimumScaleFactor = 0.78;
             label.translatesAutoresizingMaskIntoConstraints = NO;
             [stackView addArrangedSubview:label];
         }];
